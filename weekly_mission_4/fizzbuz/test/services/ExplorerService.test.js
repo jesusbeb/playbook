b879e521d156ed997f999d36f87d4f1c4d0e0498
jesusbeb <jesusbelb@gmail.com>
2022-05-04 18:53:02 -0500

    Actualizando ExplorerService.test.js con error

--- weekly_mission_4/fizzbuz/test/services/ExplorerService.test.js
+++ weekly_mission_4/fizzbuz/test/services/ExplorerService.test.js
@@ -4,7 +4,7 @@ describe("Test para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una mision", () => {
         const explorers = [{ mission: "node" }]; // esto es lo que ya sabemos que vamos a recibir del archivo json que vamos a leer
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
-        expect(explorersInNode.length).toBe(1);
+        expect(explorersInNode.length).toBe(0);
     });
 })
 
