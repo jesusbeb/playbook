76ebeb800b9c20f46b212e0a397de9252a18f3f5
jesusbeb <jesusbelb@gmail.com>
2022-05-05 18:57:23 -0500

    forEach para calcular la suma de todos los elementos de una lista

---
+++ weekly_mission_2/1_operadores/operadores02.js
@@ -0,0 +1,6 @@
+// Ejemplo 2: for Each para calcular la suma de todos los elementos de una lista
+let sum = 0;    // se declara variable sum igual a cero
+const numbers2 = [1, 2, 3, 4, 5];   // se declara una constante que contiene la lista
+numbers2.forEach(num => sum += num)     // se realiza la suma
+console.log("Ejemplo 2: Cálculo de la suma de los elementos de la lista")   // se imprime este texto
+console.log(sum)    // se imprime el valor de la variable sum
