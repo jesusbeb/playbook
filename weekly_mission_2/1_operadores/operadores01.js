6788d42d7a68c6c5b3c9ff6d707dd1eb86659388
jesusbeb <jesusbelb@gmail.com>
2022-05-05 18:56:13 -0500

    for Each para imprimir elementos de una lista

---
+++ weekly_mission_2/1_operadores/operadores01.js
@@ -0,0 +1,4 @@
+// Ejemplo 1: for Each para imprimir elementos de una lista
+const numbers = [1, 2, 3, 4, 5];    // se crea una lista
+console.log("Ejemplo 1: Imprimiendo los elementos de una lista")    //imprime el texto entre parentesis
+numbers.forEach(num => console.log(num))    // imprime el contenido de la lista
