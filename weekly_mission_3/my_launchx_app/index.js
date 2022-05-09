945d19ed562ce424abf9069380fc2620cfe2aa9e
jesusbeb <jesusbelb@gmail.com>
2022-05-09 13:35:00 -0500

    I Crear nuevo proyectos de JS, II Agregar dependencias. III Creación de clase y modularización

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,4 @@
+const MissionCommander = require('./app/missionCommander'); // importa la clase MissionCommander del archivo missionCommander
+
+const carlo=new MissionCommander("Carlo");
+console.log(carlo.name)
