b8605bee8dc70ba9007840556202fff21cea57dc
jesusbeb <jesusbelb@gmail.com>
2022-05-11 08:44:40 -0500

    Segundo requerimiento usando TDD

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -2,13 +2,13 @@ const User= require('./../../app/models/user')
 
 describe("Unit Tests for User class", () => {
     test('Create an User object', () => {
-        const user= new User(1, "jesusBeB", "Jesus", "Bio", "dateCreated", "lastUpdated")
+        const user= new User(1, "jesusBeB", "Jesus", "Bio")
 
         expect(user.id).toBe(1)
         expect(user.username).toBe("jesusBeB")
         expect(user.name).toBe("Jesus")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dateCreated).not.toBeUndefined()    // verifica que el valor no sea undefine
+        expect(user.lastUpdated).not.toBeUndefined()
     });
 })
