0755d3be193411f227a6ac36f553a6633deb1295
jesusbeb <jesusbelb@gmail.com>
2022-05-11 08:58:08 -0500

    Tercer requerimiento usando TDD

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -11,4 +11,12 @@ describe("Unit Tests for User class", () => {
         expect(user.dateCreated).not.toBeUndefined()    // verifica que el valor no sea undefine
         expect(user.lastUpdated).not.toBeUndefined()
     });
+
+    test('Add getters', () => {
+        const user= new User(1, "jesusBeB", "Jesus", "Bio")
+        expect(user.getUsername).toBe("jesusBeB")
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
+    });
 })
