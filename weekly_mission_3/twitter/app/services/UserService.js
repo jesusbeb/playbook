447ce6ce160e931ea30c7e45d0cf1649935de32e
jesusbeb <jesusbelb@gmail.com>
2022-05-11 14:25:56 -0500

    Sprint2. Agregar servicios. Requerimiento 2

--- weekly_mission_3/twitter/app/services/UserService.js
+++ weekly_mission_3/twitter/app/services/UserService.js
@@ -4,5 +4,9 @@ class UserService{
     static create(id, username, name){  // metodo estatico que recibe 3 parametros
         return new User(id, username, name, "sin bio") // los 3 parametros recibidos los envia a la clase importada User y envia el parametro "bio" con informacion por default
     }
+
+    static getInfo(usuario){   // getInfo("usuario" cualquier nombre descriptivo para el parametro que recibe) 
+        return [usuario.id, usuario.username, usuario.name, usuario="sin bio"];
+    }
 }
 module.exports= UserService;
--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -10,4 +10,15 @@ describe('Test for UserService', () => {
         expect(user.name).toBe("Jesus");
         expect(user.bio).not.toBeUndefined();
     })
+
+    test('2. Get all user data in a list', () => {
+        const user= UserService.create(1, "jesusBeB", "Jesus")  // se instancia objeto "user" a partir de la clase UserService mediante un metodo "create" y que a su vez hereda de la clase User
+        const userInfoInList= UserService.getInfo(user)   // userInfoList sera una lista que recibira la informacion al enviar el parametro "user" al metodo getInfo de la clase UserService
+
+        expect(userInfoInList[0]).toBe(1);        // se testean los elementos de la lista
+        expect(userInfoInList[1]).toBe("jesusBeB");
+        expect(userInfoInList[2]).toBe("Jesus");
+        expect(userInfoInList[3]).toBe("sin bio");
+    })
+
 })
