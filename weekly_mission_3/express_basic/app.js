03483386fa2058688620f7eed407bceb7516c9a0
jesusbeb <jbeltranb@outlook.com>
2022-06-02 17:16:24 -0500

    Agregando comentarios educativos

--- weekly_mission_3/express_basic/app.js
+++ weekly_mission_3/express_basic/app.js
@@ -1,11 +1,9 @@
-const express = require('express')  // Usando objeto express
-
-const app = express()   // App de Express
-
+const express = require('express')  // importando el modulo express que es una dependencia. Usando objeto express
+const app = express()   // Sintaxis de express en la documentacion. Se inicializa el objeto
 const port = 3000   // Puerto en que vamos a ver nuestra app: localhost:3000
 
-// path inicial, respondera a la 
-// url localhost:3000
+// '/' puede ser cualquier URL, si se queda asi como esta, responderia al directorio raiz de la url localhost:3000
+// "req" significa que alguien le mandara un requerimiento, request. "res" significa que le dara una respuesta, response
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -20,17 +18,17 @@ app.get('/launchx', (req, res) => {
 // localhost:3000/explorersInNode
 app.get('/explorersInNode', (req, res) => {
     const explorer = {name: "Explorer", msg: "Hello"} // se crea un objeto con atributos
-    res.send(explorer)
+    res.send(explorer) // se envia el objeto creado como respuesta
 })
 
 /*
-Agrega una nueva ruta, indica que recibirá un parámetro: /explorers/:explorerName, esto indicará que :explorerName será un valor enviado por la url.
+Agrega una nueva ruta, indica que recibirá un parámetro: /explorers/:explorerName, esto indicará que :explorerName será un valor enviado por la url
 El objeto req contiene la propiedad params, esta propiedad contiene los Query Params (parámetros) enviados por la url. (req.params)
 */
 // Query Params: Recibir parámetros por la url
 // http://localhost:3000/explorers/jesus
 // req.params= {"explorerName":"jesus"}
-app.get('/explorers/:exploreName', (req, res) => {
+app.get('/explorers/:explorerName', (req, res) => {     // devolvera un objeto con la propiedad explorerName y su valor sera el que se envie por url p.e. http://localhost:3000/explorers/Jesus
     res.send(req.params)
 })
 
